5c075ff98dd0aa463ab354438c46163435d7fb70
rishi <chiragt1805@gmail.com>
2023-05-23 20:43:04 +0530

    code cleaning in selenium

--- pytorch-implementation/buy_sell.py
+++ pytorch-implementation/buy_sell.py
@@ -20,15 +20,6 @@ def run_selenium():
     driver.get("https://in.tradingview.com/chart/?symbol=NSE%3AINFY")
     driver.maximize_window()
     time.sleep(5)
-    # driver.find_element(By.CLASS_NAME, 'tv-header__offer-button-title').click()
-    # time.sleep(2)
-    # Login button click
-    # driver.find_element(By.CLASS_NAME, 'tv-header__user-menu-button').click()
-    # time.sleep(3)
-    # Sign in button click 
-    # //*[@id="overlap-manager-root"]/div[2]/span/div[1]/div/div/div/div/button[1]
-    # #overlap-manager-root > div:nth-child(2) > span > div.menu-_s3Vh8P3.menuWrap-biWYdsXC > div > div > div > div > button:nth-child(1)
-    # /html/body/div[8]/div[2]/span/div[1]/div/div/div/div/button[1]
     def buy(quantity):
         print("In buy function")
         driver.find_element("xpath","/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tr[1]/td[2]/div/div[2]/div[1]/div[2]/div[1]/div[2]/div[2]").click()
@@ -71,11 +62,6 @@ def run_selenium():
     # Click on submit
     driver.find_element(By.CLASS_NAME,"tv-button__loader").click()
     time.sleep(2)
-    # driver.find_element(By.CLASS_NAME,"tv-header-search-container").click()
-    # time.sleep(2)
-    # driver.find_element(By.CLASS_NAME,"symbolTitle-DPHbT8fH").click()
-    # time.sleep(7)
-    # driver.find_element(By.CLASS_NAME,"titleText-FR_Ayazi").click()
     driver.find_element("xpath","//*[@id='footer-chart-panel']/div[2]/button[1]").click()
     time.sleep(2) 
     #object of ActionChains
@@ -117,28 +103,9 @@ def run_selenium():
             with open("./parameters.json", "w") as outfile:
                 json.dump(json_object, outfile)
         else:
-            pass
-            # print("Some error occurred in the hole")
+            print("Buy/Sell failed")
         print("Before time sleep")
         time.sleep(20)
 run_selenium()
 
-    # # Hover on chart
-    # n = driver.find_element("xpath","/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tr[1]/td[2]/div/canvas[2]")
-    # l = driver.find_element("xpath","/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tr[1]/td[2]/div/canvas[2]")
-    # a.move_to_element(n).perform()
-    # a.context_click(l).perform()
-    # time.sleep(3)
-    # driver.find_element("xpath","//*[@id=\"overlap-manager-root\"]/div/div[1]/div/div/table/tbody/tr[11]/td[2]/div/span[1]").click()
-    # time.sleep(3)
-    # driver.find_element("xpath","//*[@id=\"overlap-manager-root\"]/div/div[1]/div/div/table/tbody/tr[12]/td/div[1]/div/div/table/tbody/tr[5]/td[2]/div").click()
-    # time.sleep(3)
-    # textBox = driver.find_element("xpath","//*[@id=\"overlap-manager-root\"]/div/div/div[1]/div/div/div/div[5]/div[1]/div/span/span/span[1]/input")
-    # textBox.click()
-    # textBox.send_keys(Keys.COMMAND + 'a')
-    # time.sleep(1)
-    # textBox.send_keys(Keys.DELETE)
-    # # textBox.send_keys("10")
-    # textBox.send_keys(str(shares_held[0]))
-    # driver.find_element("xpath","//*[@id=\"overlap-manager-root\"]/div/div/div[1]/div/div/div/div[6]/button/div/span[2]").click()
-
+   
